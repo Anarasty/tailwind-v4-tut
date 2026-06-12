6c8d4dbcc72095b312636e1da38ac7b0437cf0cd
Anarasty <murloc.ninja16@gmail.com>
2026-06-12 22:57:23 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+     <div className="min-h-screen bg-teal-100">
+      <div className="flex justify-between bg-teal-500 px-6 py-4 text-teal-100">
+        <h1>Tailwind</h1>
+        <nav className="flex gap-4">
+          <a href="#">Home</a>
+          <a href="#">Blog</a>
+          <a href="#">Contact</a>
+        </nav>
+      </div>
+     </div>
+    </>
+  );
+}
+
+export default App;
