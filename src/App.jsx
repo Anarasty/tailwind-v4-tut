3cfc18c45e5ba945345d13b89e8a49fc5021cccc
Anarasty <murloc.ninja16@gmail.com>
2026-06-15 09:56:33 +0300

    testing flex grow

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-teal-100">
+      <div className="min-h-screen bg-teal-100 flex flex-col">
         <div className="flex justify-between bg-teal-500 px-6 py-4 text-teal-100 mb-8">
           <h1 className="text-lg font-bold tracking-widest">Tailwind</h1>
           <nav className="flex gap-4">
@@ -11,7 +11,7 @@ function App() {
           </nav>
         </div>
 
-        <div className="space-y-8">
+        <div className="space-y-8 flex-grow">
           <section className="text-center">
             <h2 className="font-semibold text-2xl mb-4">Welcome</h2>
             <p>This is the main content area of the webside</p>
@@ -22,18 +22,40 @@ function App() {
               Features
             </h2>
             <div className="grid grid-cols-1 gap-6 px-6 sm:grid-cols-2 md:grid-cols-3">
-              <div className="bg-teal-300 p-4">Feature 1</div>
-              <div className="bg-teal-300 p-4">Feature 2</div>
-              <div className="bg-teal-300 p-4">Feature 3</div>
-              <div className="bg-teal-300 p-4">Feature 4</div>
-              <div className="bg-teal-300 p-4">Feature 5</div>
-              <div className="bg-teal-300 p-4">Feature 6</div>
-              <div className="bg-teal-300 p-4">Feature 7</div>
-              <div className="bg-teal-300 p-4">Feature 8</div>
-              <div className="bg-teal-300 p-4">Feature 9</div>
+              <div className="rounded-xl bg-teal-300 p-4 hover:bg-teal-300/80 hover:cursor-pointer hover:text-teal-100 hover:shadow-lg">
+                Feature 1
+              </div>
+              <div className="rounded-xl bg-teal-300 p-4 hover:bg-teal-300/80 hover:cursor-pointer hover:text-teal-100 hover:shadow-lg">
+                Feature 2
+              </div>
+              <div className="rounded-xl bg-teal-300 p-4 hover:bg-teal-300/80 hover:cursor-pointer hover:text-teal-100 hover:shadow-lg">
+                Feature 3
+              </div>
+              <div className="rounded-xl bg-teal-300 p-4 hover:bg-teal-300/80 hover:cursor-pointer hover:text-teal-100 hover:shadow-lg">
+                Feature 4
+              </div>
+              <div className="rounded-xl bg-teal-300 p-4 hover:bg-teal-300/80 hover:cursor-pointer hover:text-teal-100 hover:shadow-lg">
+                Feature 5
+              </div>
+              <div className="rounded-xl bg-teal-300 p-4 hover:bg-teal-300/80 hover:cursor-pointer hover:text-teal-100 hover:shadow-lg">
+                Feature 6
+              </div>
+              <div className="rounded-xl bg-teal-300 p-4 hover:bg-teal-300/80 hover:cursor-pointer hover:text-teal-100 hover:shadow-lg">
+                Feature 7
+              </div>
+              <div className="rounded-xl bg-teal-300 p-4 hover:bg-teal-300/80 hover:cursor-pointer hover:text-teal-100 hover:shadow-lg">
+                Feature 8
+              </div>
+              <div className="rounded-xl bg-teal-300 p-4 hover:bg-teal-300/80 hover:cursor-pointer hover:text-teal-100 hover:shadow-lg">
+                Feature 9
+              </div>
             </div>
           </section>
         </div>
+
+        <footer className="bg-teal-500 py-6 text-center text-sm text-teal-100 mt-4">
+          &copy; 2026 Testing Tailwind. All rights reserved.
+        </footer>
       </div>
     </>
   );
